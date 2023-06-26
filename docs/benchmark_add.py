50430cadfba470963893a5e7d6bae4c96875a6a6
Adolph Vogel <ajvogel@gmail.com>
2023-06-26 19:48:15 +0200

    Added benchmark_add.py

---
+++ docs/benchmark_add.py
@@ -0,0 +1,29 @@
+import sys
+import pathlib
+import time
+
+from numpy import tri
+sys.path.append(str((pathlib.Path(__file__) / '..' / '..').resolve()))
+
+import casino as cs
+
+print('Running...')
+tic = time.time()
+tri1 = cs.Triangular(10, 50, 30)
+
+bla = tri1
+
+for i in range(15):
+    bla = bla + cs.Triangular(10, 50, 30)
+
+toc = time.time()
+
+# print(len(bla.w))
+
+
+# cs.printPMF(bla)
+
+print(f'Took {(toc - tic) * 1000} milliseconds')
+
+
+
