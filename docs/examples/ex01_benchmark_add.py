fde4c7fc9e510f5a3ae8e07571ccacbd5e0ddb85
Adolph Vogel <ajvogel@gmail.com>
2023-08-20 21:04:43 +0200

    Moved benchmark_add file

---
+++ docs/examples/ex01_benchmark_add.py
@@ -0,0 +1,17 @@
+import sys
+import pathlib
+import time
+import casino as cs
+
+print('Running...')
+tic = time.time()
+tri1 = cs.Triangular(10, 30, 50)
+
+bla = tri1 
+
+for i in range(25):
+    bla = bla + cs.Triangular(10, 30, 50)
+
+toc = time.time()
+
+print(f'Took {(toc - tic) * 1000} milliseconds')
