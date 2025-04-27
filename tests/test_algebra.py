6505f7bda40bfc881505ecb7f6963a246b0d5206
Adolph Vogel <ajvogel@gmail.com>
2025-04-27 11:13:00 +0200

    Added test_algebra.py

---
+++ tests/test_algebra.py
@@ -0,0 +1,60 @@
+import casino as cs
+
+
+def test_pow():
+    x = cs.Constant(3)
+    y = cs.Constant(2)
+    z = x ** y
+    assert z.sample() == 3**2
+
+def test_add():
+    x = cs.Constant(1)
+    y = cs.Constant(2)
+    z = x + y
+    assert z.sample() == 3
+
+def test_sub():
+    x = cs.Constant(1)
+    y = cs.Constant(2)
+    z = x - y
+    assert z.sample() == -1
+
+def test_mul():
+    x = cs.Constant(1)
+    y = cs.Constant(2)
+    z = x * y
+    assert z.sample() == 2
+
+def test_div():
+    x = cs.Constant(1)
+    y = cs.Constant(2)
+    z = x / y
+    assert z.sample() == 0.5
+
+def test_mod():
+    x = cs.Constant(1)
+    y = cs.Constant(2)
+    z = x % y
+    assert z.sample() == 1
+
+def test_floor_div():
+    x = cs.Constant(1)
+    y = cs.Constant(2)
+    z = x // y
+    assert z.sample() == 0
+
+def test_order_of_operations():
+    a = cs.Constant(2)
+    b = cs.Constant(3)
+    c = cs.Constant(4)
+    d = cs.Constant(5)
+
+    z = (a + b) * (c + d)
+
+    assert z.sample() == (2 + 3) * (4 + 5)
+
+    e = cs.Constant(6)
+
+    z = (((a + b) * c) + d) * e
+
+    assert z.sample() == (((2 + 3) * 4) + 5) * 6
